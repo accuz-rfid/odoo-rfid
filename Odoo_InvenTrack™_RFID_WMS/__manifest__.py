ccf81f4c0b8aeef78dac9db42d1a6f0111811c58
Seven-Hsia <seven.h@accuz.com>
2026-01-08 16:31:06 +0800

    Clean up repository and keep only InvenTrack_RFID

--- Odoo_InvenTrack™_RFID_WMS/__manifest__.py
+++ Odoo_InvenTrack™_RFID_WMS/__manifest__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Odoo_InvenTrack_RFID_WMS",
-    'summary': "Inventrack_RFID",
+    'name': "Odoo_InvenTrack™_RFID_WMS",
+    'summary': "Inventrack™_RFID",
     'description': """
-        1.出入库管理
-        2.盘点管理
-        3.LED亮灯寻物
-        4.产品信息更改
+        1.Inbound
+        2.Outbound
+        3.Inventory Adjustment
+        4.LED locating
+        5.Internal Transfer
     """,
     'author': "accuz",
-    'category': 'accuz',
+    'category': 'odoo add-ons',
     'version': '18.0',
     'depends': ['base', 'stock'],
     'data': [
